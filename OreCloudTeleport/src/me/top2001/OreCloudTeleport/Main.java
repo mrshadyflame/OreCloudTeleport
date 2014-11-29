dd11046f6eb335168025908fd8a92c62b1e13a16
top2001 <top2001@users.noreply.github.com>
2014-11-29 16:43:42 +0000

    Test Of Rewrite.

--- OreCloudTeleport/src/me/top2001/OreCloudTeleport/Main.java
+++ OreCloudTeleport/src/me/top2001/OreCloudTeleport/Main.java
@@ -2,10 +2,7 @@ package me.top2001.OreCloudTeleport;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Instrument;
 import org.bukkit.Location;
-import org.bukkit.Note;
-import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -27,30 +24,47 @@ public class Main extends JavaPlugin{
 		   Player player = (Player) sender;
 		   Location loc = player.getLocation();
 		   if(cmd.getName().equalsIgnoreCase("tp")) {
-		    if (args.length == 0) {
-		    	player.sendMessage(prefix + ChatColor.GREEN + "You haven't specified a player to teleport to!");
-	            player.playNote(loc, Instrument.PIANO, new Note(1));
-	            return true;
-		    }
-		    Player target = Bukkit.getServer().getPlayerExact(args[0]);
-		    if (target == null) {
-		    	player.sendMessage(prefix + ChatColor.GREEN + "The player, " + ChatColor.DARK_GREEN + args[0] + ChatColor.GREEN + ", is currently not online.");
-		    	player.playNote(loc, Instrument.PIANO, new Note(1));
-		    	return true;
-		    }
-		    player.teleport(target.getLocation());
-		    return true;
+			   if (args.length == 0) {
+				   player.sendMessage(prefix + ChatColor.GREEN + "Please specify a player. (/tp <Player> {TargetPlayer})");
+			   }else if (args.length == 1) {
+				   Player targetPlayer = player.getServer().getPlayerExact(args[0]);
+				   Location targetPlayerLocation = targetPlayer.getLocation();
+				   player.teleport(targetPlayerLocation);
+				   player.sendMessage(prefix + ChatColor.GREEN + "Now teleporting...");
+			   }else if (args.length == 2) {
+				   Player targetPlayer = player.getServer().getPlayerExact(args[0]);
+				   Player targetPlayer1 = player.getServer().getPlayerExact(args[1]);
+				   Location targetPlayer1Location = targetPlayer1.getLocation();
+				   targetPlayer.teleport(targetPlayer1Location);
+				   player.sendMessage(prefix + ChatColor.GREEN + "Now teleporting...");
+			   }
+			   Player targetPlayer = Bukkit.getServer().getPlayerExact(args[0]);
+			   if (targetPlayer == null) {
+				   player.sendMessage(prefix + ChatColor.GREEN + "The player, " + ChatColor.DARK_GREEN + args[0] + ChatColor.GREEN + ", is currently not online.");
+			   }
+			   Player targetPlayer1 = Bukkit.getServer().getPlayerExact(args[1]);
+			   if (targetPlayer1 == null) {
+				   player.sendMessage(prefix + ChatColor.GREEN + "The player, " + ChatColor.DARK_GREEN + args[1] + ChatColor.GREEN + ", is currently not online.");
+			   }
 		   }
 		   // Sets Spawn.
 		   if(cmd.getName().equalsIgnoreCase("setspawn")) {
-			   player.getWorld().setSpawnLocation(player.getLocation().getBlockX(), player.getLocation().getBlockY(), player.getLocation().getBlockZ());
-			   player.sendMessage(prefix + ChatColor.GREEN + "Spawn location created.");
+			   if(sender.hasPermission("orecloudteleport.setspawn")){
+				   player.getWorld().setSpawnLocation(player.getLocation().getBlockX(), player.getLocation().getBlockY(), player.getLocation().getBlockZ());
+				   player.sendMessage(prefix + ChatColor.GREEN + "Spawn location created.");
+			   }else{
+				   player.sendMessage(prefix + ChatColor.RED + "Sorry, you don't have permission to perform this command.");
+			   }
 		   }
-		   // Teleports Player To Spawn.
+		   // Teleport Player To Spawn.
 		   if (cmd.getName().equalsIgnoreCase("spawn")) {
-			   player.teleport(player.getWorld().getSpawnLocation());
-			   player.sendMessage(prefix + ChatColor.GREEN + "Now sending you to spawn...");
-			   return true;
+			   if(sender.hasPermission("orecloudteleport.spawn")){
+				   player.teleport(player.getWorld().getSpawnLocation());
+				   player.sendMessage(prefix + ChatColor.GREEN + "Now sending you to spawn...");
+			   }else{
+				   player.sendMessage(prefix + ChatColor.RED + "Sorry, you don't have permission to perform this command.");
+				   return true;
+			   }
 		   }
 		   }
 		return false;
